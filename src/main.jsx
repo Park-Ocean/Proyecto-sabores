9c4ac7518a834f28cedef4242465b4504c048db9
Park-Ocean <bastianapablazavega@gmail.com>
2025-11-05 20:31:33 -0300

    Se limpio el proyecto, se instalo y configuro react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Provider } from "@/components/ui/provider"
+import { BrowserRouter } from 'react-router-dom';  // Importa BrowserRouter
+import React from "react"
+import App from "./App"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 )
