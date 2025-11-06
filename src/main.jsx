65a2681ee2ff09527d570b760a9c1a473be8bb64
FrancBarr <64182282+aPoncho@users.noreply.github.com>
2025-11-06 14:09:46 -0300

    DELETED: auths, admin page

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext"; 
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <ColorModeScript />
-      <AuthProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </AuthProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
