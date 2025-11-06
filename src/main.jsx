a987f88cfcd163d542e6b6735be0a2caba161a10
Park-Ocean <bastianapablazavega@gmail.com>
2025-11-06 14:17:31 -0300

    Configuracion authprovider

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ColorModeScript />
     <ChakraProvider>
-      <ColorModeScript />
+      <AuthProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
+      </AuthProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
